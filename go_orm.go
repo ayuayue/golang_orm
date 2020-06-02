335e68b2842987fc96a8d87d115a4e71c6c8695e
ayuayue <1401262639@qq.com>
2020-06-02 20:41:14 +0800

    第七天,实现数据库简单迁移,完结

--- go_orm.go
+++ go_orm.go
@@ -2,28 +2,31 @@ package golang_orm
 
 import (
 	"database/sql"
+	"fmt"
 	"golang_orm/dialect"
 	"golang_orm/log"
 	"golang_orm/session"
+	"strings"
 )
 
 type Engine struct {
 	db      *sql.DB
 	dialect dialect.Dialect
 }
-type TxFunc func(*session.Session)(interface{},error)
-func (e *Engine) Transaction(f TxFunc)(result interface{}, err error){
+type TxFunc func(*session.Session) (interface{}, error)
+
+func (e *Engine) Transaction(f TxFunc) (result interface{}, err error) {
 	s := engine.NewSession()
 	if err := s.Begin(); err != nil {
-		return nil,err
+		return nil, err
 	}
 	defer func() {
-		if p := recover();p != nil {
+		if p := recover(); p != nil {
 			_ = s.Rollback()
 			panic(p)
-		}else if err != nil {
+		} else if err != nil {
 			_ = s.Rollback()
-		}else {
+		} else {
 			err = s.Commit()
 		}
 	}()
@@ -59,4 +62,49 @@ func (engine *Engine) Close() {
 
 func (engine *Engine) NewSession() *session.Session {
 	return session.New(engine.db, engine.dialect)
+}
+func difference(a []string, b []string) (diff []string) {
+	mapB := make(map[string]bool)
+	for _, v := range b {
+		mapB[v] = true
+	}
+	for _, v := range a {
+		if _, ok := mapB[v]; !ok {
+			diff = append(diff, v)
+		}
+	}
+	return
+}
+func (engine *Engine) Migrate(value interface{}) error {
+	_, err := engine.Transaction(func(s *session.Session) (result interface{}, err error) {
+		if !s.Model(value).HasTable() {
+			log.Infof("table %s doesn't exist", s.RefTable().Name)
+			return nil, s.CreateTable()
+		}
+		table := s.RefTable()
+		rows, _ := s.Raw(fmt.Sprintf("SELECT * FROM %s LIMIT 1", table.Name)).QueryRows()
+		columns, _ := rows.Columns()
+		addCols := difference(table.FieldNames, columns)
+		delCols := defference(columns, table.FieldNames)
+		log.Infof("add cols %v, deleted cols %v", addCols, delCols)
+
+		for _, col := range addCols {
+			f := table.GetField(col)
+			sqlStr := fmt.Sprintf("ALTER TABLE %s ADD COLUMN %s %s", table.Name, f.Name, f.Type)
+			if _, err := s.Raw(sqlStr).Exec(); err != nil {
+				return
+			}
+		}
+		if len(delCols) == 0 {
+			return
+		}
+		tmp := "tmp_" + table.Name
+		fieldStr := strings.Join(table.FieldNames, ", ")
+		s.Raw(fmt.Sptintf("CREATE TABEL %s AS SELECT %s FROM %s;", tmp, fieldStr, table.Name))
+		s.Raw(fmt.Sptintf("DROP TABLE %s;", table.Name))
+		s.Raw(fmt.Sptintf("ALTER TABLE %s RENAME TO %s;", tmp, table.Name))
+		_, err = s.Exec()
+		return
+	})
+	return err
 }
